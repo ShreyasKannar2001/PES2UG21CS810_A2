7d93b88a85ab694dd00ab8a406752202abc0ada0
ShreyasKannar2001 <125908302+ShreyasKannar2001@users.noreply.github.com>
2023-02-20 23:14:31 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,8 +3,8 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, World\n";
+  cout << "Hello, I am Shreyas\n";
   cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run\n";
+  cout << "I have successfully Completing a Assignment\n";
   return 0;
 }
